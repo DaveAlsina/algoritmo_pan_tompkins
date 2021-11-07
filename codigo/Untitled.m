bf96216f548e98ab53a0dd9265794169c0528de7
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-11-06 19:36:12 -0500

    adelantos del informe y nuevos datos, todo prácticamente finalizado.

--- codigo/Untitled.m
+++ codigo/Untitled.m
@@ -1,7 +1,5 @@
 signal1 = load('../data/MLII/1 NSR/100m (0).mat');
 signal2 = load('../data/MLII/1 NSR/100m (1).mat');
-signal3 = load('../data/MLII/1 NSR/100m (2).mat');
 
 ecg = [signal1.val signal2.val];
-
 Fs = 360;
